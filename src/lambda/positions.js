a3c8f1d493c7f64842338e884c02c16f7c8a0cd6
Chris Schweda <cschweda@gmail.com>
2019-01-12 13:20:07 -0600

    update: install vuetify

---
+++ src/lambda/positions.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const btoa = require("btoa");
+require("dotenv").config();
+
+exports.handler = (event, context, callback) => {
+  var basicAuth =
+    "Basic " +
+    btoa(process.env.METRA_ACCESS_KEY + ":" + process.env.METRA_SECRET_KEY);
+  axios
+    .get("https://gtfsapi.metrarail.com/gtfs/positions", {
+      headers: {
+        "cache-control": "no-cache",
+        Authorization: basicAuth
+      }
+    })
+    .then(res => {
+      callback(null, { statusCode: 200, body: JSON.stringify(res.data) });
+    })
+    .catch(err => {
+      callback(err);
+    });
+};
