c35f9f5be8377f2c1e76cdfa93526a6a475ab175
Chris Schweda <cschweda@gmail.com>
2019-01-12 15:50:02 -0600

    fix: add google analytics

--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
-import '@babel/polyfill'
+import "@babel/polyfill";
 import Vue from "vue";
-import './plugins/vuetify'
+import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 import TreeView from "vue-json-tree-view";
 Vue.use(TreeView);
+
+import Vue from "vue";
+import VueAnalytics from "vue-analytics";
+
+Vue.use(VueAnalytics, {
+  id: "UA-132350618-1",
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
+});
 
 Vue.config.productionTip = false;
 
