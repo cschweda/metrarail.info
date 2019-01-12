6fffdf17ca9cf7d035f9c5e9a2eaab121b805764
Chris Schweda <cschweda@gmail.com>
2019-01-12 13:49:34 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,10 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>METRARAIL.</span>
+        <span class="font-weight-light">INFO</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>open_in_new</v-icon>
-      </v-btn>
     </v-toolbar>
 
     <v-content>
@@ -23,13 +15,12 @@
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-  data () {
+  name: "App",
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,62 @@
 <template>
-  <HelloWorld />
+  <div>
+    <h1>Alerts</h1>
+    <div v-if="isLoadingAlerts">
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </div>
+    <div
+      v-else
+      v-for="(alert) in alerts"
+      :key="alert.id"
+    >{{alert.alert.header_text.translation[0].text}}</div>
+    <h1>Positions</h1>
+    <div v-if="isLoadingPositions">
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </div>
+    <div v-else v-for="(position) in positions" :key="position.id">{{position.vehicle.trip.trip_id}}</div>
+    <h1>Trip Updates</h1>
+    <div v-if="isLoadingTripUpdates">
+      <v-progress-circular indeterminate color="primary"></v-progress-circular>
+    </div>
+    <div v-else v-for="(tripUpdate) in tripUpdates" :key="tripUpdate.id">{{tripUpdate.id}}</div>
+    <tree-view :data="tripUpdates" :options="{maxDepth: 3}"></tree-view>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+// @ is an alias to /src
+import axios from "axios";
 
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+export default {
+  created() {
+    this.isLoadingAlerts = true;
+    this.isLoadingPositions = true;
+    this.isLoadingTripUpdates = true;
+    axios.get("/.netlify/functions/tripUpdates").then(res => {
+      this.tripUpdates = res.data;
+      this.isLoadingTripUpdates = false;
+    });
+    axios.get("/.netlify/functions/alerts").then(res => {
+      this.alerts = res.data;
+      this.isLoadingAlerts = false;
+    });
+
+    axios.get("/.netlify/functions/positions").then(res => {
+      this.positions = res.data;
+      this.isLoadingPositions = false;
+    });
+  },
+  data() {
+    return {
+      tripUpdates: "",
+      alerts: "",
+      positions: "",
+      isLoadingAlerts: false,
+      isLoadingPositions: false,
+      isLoadingTripUpdates: false
+    };
+  },
+  name: "home",
+  components: {}
+};
 </script>
